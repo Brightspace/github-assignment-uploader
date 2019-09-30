3c85e8a6fb350640a0fc7ddb8e3cecea7fcfeb6b
Diljot Garcha <diljot.garcha@d2l.com>
2019-09-30 15:16:50 -0400

    Start adding unit tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,14 +1,29 @@
+// index.test.js
+
 const nock = require('nock')
+
 // Requiring our app implementation
 const myProbotApp = require('..')
 const { Probot } = require('probot')
-// Requiring our fixtures
-const payload = require('./fixtures/issues.opened')
+
+// Fixtures and outputs
+const payloadIssueOpen = require('./fixtures/issues.opened')
 const issueCreatedBody = { body: 'Thanks for opening this issue!' }
+
+const payloadCheckRunCreate = require('./fixtures/check_run.created')
+const payloadCheckRunFail = require('./fixtures/check_run.failed')
+const payloadCheckRunCancel = require('./fixtures/check_run.canceled')
+const payloadCheckRunPass = require('./fixtures/check_run.passed')
+const newCheckRunOutput = {
+  name: 'Visual Difference Tests'
+}
+const buildFailedCommentBody = {
+  body: 'Hey there! It looks like your Pull Request build failed.'
+}
 
 nock.disableNetConnect()
 
-describe('My Probot app', () => {
+describe('Visual-Difference Bot', () => {
   let probot
 
   beforeEach(() => {
@@ -16,31 +31,77 @@ describe('My Probot app', () => {
     // Load our app into probot
     const app = probot.load(myProbotApp)
 
-    // just return a test token
+    // Just return a test token
     app.app = () => 'test'
   })
 
-  test('creates a comment when an issue is opened', async () => {
+  // Testing receiving a sample event
+  test('parsing data on a sample event', async () => {
     // Test that we correctly return a test token
     nock('https://api.github.com')
       .post('/app/installations/2/access_tokens')
       .reply(200, { token: 'test' })
 
-    // Test that a comment is posted
+    // Test that a comment is posted (example of an event)
     nock('https://api.github.com')
-      .post('/repos/hiimbex/testing-things/issues/1/comments', (body) => {
+      .post('/repos/BrightspaceHypermediaComponents/activities/issues/1/comments', (body) => {
         expect(body).toMatchObject(issueCreatedBody)
         return true
       })
       .reply(200)
 
     // Receive a webhook event
-    await probot.receive({ name: 'issues', payload })
+    await probot.receive({ name: 'issues', payload: payloadIssueOpen })
+  })
+
+  // Check that we can process the receipt of a check_run.created event.
+  test('creating a check-run when Travis PR build is created', async () => {
+    // Test for POSTing a check_run
+    nock('https://api.github.com')
+      .post('/repos/BrightspaceHypermediaComponents/activities/check-runs')
+      .reply(200, newCheckRunOutput)
+
+    // Receive a webhook event
+    await probot.receive({ name: 'check_run', payload: payloadCheckRunCreate })
+  })
+  // Check that we can process the receipt of a check_run.completed failure event.
+  test('updating a check-run to failed when visual diff tests fail in Travis', async () => {
+    // Test for POSTing a check_run
+    nock('https://api.github.com')
+      .post('/repos/BrightspaceHypermediaComponents/activities/check-runs')
+      .reply(200, newCheckRunOutput)
+
+    // Test that a comment is posted (example of an event)
+    nock('https://api.github.com')
+      .post('/repos/BrightspaceHypermediaComponents/activities/issues/352/comments', (body) => {
+        expect(body.body.includes(buildFailedCommentBody.body))
+        return true
+      })
+      .reply(200)
+
+    // Receive a webhook event
+    await probot.receive({ name: 'check_run', payload: payloadCheckRunFail })
+  })
+
+  // Check that we can process the receipt of a check_run.completed cancelled event.
+  test('updating a check-run to cancelled when visual diff tests are cancelled in Travis', async () => {
+    // Test for POSTing a check_run
+    nock('https://api.github.com')
+      .post('/repos/BrightspaceHypermediaComponents/activities/check-runs')
+      .reply(200, newCheckRunOutput)
+
+    // Receive a webhook event
+    await probot.receive({ name: 'check_run', payload: payloadCheckRunCancel })
+  })
+
+  // Check that we can process the receipt of a check_run.completed cancelled event.
+  test('updating a check-run to success when visual diff tests pass in Travis', async () => {
+    // Test for POSTing a check_run
+    nock('https://api.github.com')
+      .post('/repos/BrightspaceHypermediaComponents/activities/check-runs')
+      .reply(200, newCheckRunOutput)
+
+    // Receive a webhook event
+    await probot.receive({ name: 'check_run', payload: payloadCheckRunPass })
   })
 })
-
-// For more information about testing with Jest see:
-// https://facebook.github.io/jest/
-
-// For more information about testing with Nock see:
-// https://github.com/nock/nock
