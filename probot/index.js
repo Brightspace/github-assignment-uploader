29bd1560dc34136494b46ce3f92f246198bd1a4c
Diljot Garcha <Diljot.Garcha@D2L.com>
2019-11-11 16:02:24 -0500

    Finish index.js

--- probot/index.js
+++ probot/index.js
@@ -1,5 +1,12 @@
 // index.js
 
+const got = require('got')
+const fs = require('fs')
+const path = require('path')
+
+const INFO_PREFIX = '[INFO] '
+const ERROR_PREFIX = '[ERROR] '
+
 // Globals
 var theApp = null
 
@@ -8,15 +15,14 @@ var theApp = null
  */
 module.exports = app => {
   theApp = app
-  onStart()
 }
 
 async function getContext() {
   return await theApp.auth()
 }
 
-async function getContext(installID) {
-  return await theApp.auth(installID)
+async function getContext(installation_id) {
+  return await theApp.auth(installation_id)
 }
 
 async function getInstallationID(username) {
@@ -36,8 +42,57 @@ async function getInstallationID(username) {
   return result
 }
 
-// /async function listReposForUser( )
+async function listReposForUser(username) {
+  const github = await getContext(await getInstallationID(username));
+  const output = await github.apps.listRepos()
+  let result = []
 
-async function onStart() {
-  console.log(await getInstallationID("DiljotSG"))
+  const repos = output.data.repositories
+   
+  for (const element of repos) {
+    result.push(element.full_name)
+  }
+
+  return result
+}
+
+async function getRepoArchive(username, repo_name) {
+  try {
+    let buffer = []
+
+    const params = {
+      owner: username,
+      repo: repo_name,
+      archive_format: 'zipball',
+      ref: ''
+    }
+
+    const github = await getContext(await getInstallationID(username));
+    const url = (await github.repos.getArchiveLink(params)).url
+
+    await new Promise((resolve, reject) => {
+      const stream = got.stream(url)
+
+      stream.on('data', (d) => {
+        buffer.push(d)
+      })
+
+      stream.on('end', () => {
+        buffer  = Buffer.concat(buffer)
+        resolve()
+      })
+    })
+    
+    return buffer
+
+  } catch (error) {
+    console.log(`${ERROR_PREFIX}${error}`)
+  }
+}
+
+async function getPublicURL() {
+  const github = await getContext()
+  const output = await github.apps.getAuthenticated()
+
+  return output.data.html_url
 }
