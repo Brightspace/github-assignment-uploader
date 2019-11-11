e628e20fcfdf24485710e7295b277524cd6a266f
Diljot Garcha <Diljot.Garcha@D2L.com>
2019-11-11 14:09:27 -0500

    Start writing index.js

--- probot/index.js
+++ probot/index.js
@@ -1,10 +1,43 @@
 // index.js
 
+// Globals
+var theApp = null
+
 /**
  * @param {import('probot').Application} app
  */
 module.exports = app => {
-  app.on(`*`, async context => {
-      console.log("Triggered")
-    })
+  theApp = app
+  onStart()
+}
+
+async function getContext() {
+  return await theApp.auth()
+}
+
+async function getContext(installID) {
+  return await theApp.auth(installID)
+}
+
+async function getInstallationID(username) {
+  let result = 0
+  const github = await getContext()
+  const output = await github.apps.listInstallations()
+
+  for (const element of output.data) {
+    const login = element.account.login
+
+    if (login == username) {
+      result = element.id
+      break
+    }
+  }
+
+  return result
+}
+
+// /async function listReposForUser( )
+
+async function onStart() {
+  console.log(await getInstallationID("DiljotSG"))
 }
