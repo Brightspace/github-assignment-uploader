aab5c284f577763802a68516f21e914ffab96870
Diljot Garcha <Diljot.Garcha@D2L.com>
2019-11-12 14:15:11 -0500

    Progress?

--- handler.js
+++ handler.js
@@ -1,4 +1,9 @@
 // handler.js
+const { Probot } = require('probot')
 const serverless = require('serverless-http');
-const appFn = require('./')
-module.exports.probot = serverless(appFn)
+
+const probotApp = require('./')
+myProbot = new Probot({})
+const app = myProbot.load(probotApp)
+
+module.exports.probot = serverless(app)
